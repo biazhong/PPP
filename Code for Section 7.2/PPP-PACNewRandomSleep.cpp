f452216bbffa4eb23b923524b1472b2154eb35d0
biazhong <45530078+biazhong@users.noreply.github.com>
2020-10-27 14:20:34 +0800

    Update PPP-PACNewRandomSleep.cpp

--- Code for Section 7.2/PPP-PACNewRandomSleep.cpp
+++ Code for Section 7.2/PPP-PACNewRandomSleep.cpp
@@ -27,6 +27,7 @@ using namespace std;
 using namespace std::this_thread;
 using namespace std::chrono; 
 
+//method used to genereate exponential random variables
 double exprand(double lambda){
     double u;
     
@@ -35,7 +36,7 @@ double exprand(double lambda){
     return -log(1- u) / lambda;
 }
 
-
+//struct that contains all the information of an alternative
 typedef struct{
     int label;
     int sampleSize;
@@ -44,13 +45,13 @@ typedef struct{
     int batchsize;
 } alt;
 
-
+//method used by the master to send out tasks to workers
 void master_send_out_tasks(vector<alt>* outgoing_alts, int outgoing_rank){
     MPI_Send((void*)outgoing_alts->data(),outgoing_alts->size()*sizeof(alt), MPI_BYTE, outgoing_rank, 0, MPI_COMM_WORLD);
 }
 
 
-
+//method used by the master to receive tasks from workers
 void master_receive_tasks(vector<alt>* incoming_alts){
     MPI_Status status;
     int incoming_rank;
@@ -65,12 +66,12 @@ void master_receive_tasks(vector<alt>* incoming_alts){
     MPI_Recv((void*)incoming_alts->data(),incoming_size, MPI_BYTE,incoming_rank,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
 }
 
-
+//method used by workers to send out tasks to the master
 void worker_send_out_tasks(vector<alt>* outgoing_alts){
     MPI_Send((void*)outgoing_alts->data(), outgoing_alts->size()*sizeof(alt), MPI_BYTE, 0, 0 ,MPI_COMM_WORLD);
 }
 
-
+//method used by workers to receive tasks from the master
 void worker_receive_tasks(vector<alt>* incoming_alts){
     MPI_Status status;
     int incoming_rank = 0;
@@ -83,7 +84,7 @@ void worker_receive_tasks(vector<alt>* incoming_alts){
     
     MPI_Recv((void*)incoming_alts->data(),incoming_size, MPI_BYTE,incoming_rank,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
 }
-
+//method used to generate observations for the three-stage buffer allocation problem
 void generate_obv (double* _sim, int label){
     int RB = 50;    //Input Parameter: Problem parameter
     vector<int> x_disc(5);
@@ -171,7 +172,7 @@ void generate_obv (double* _sim, int label){
 
     *_sim = ((double)(_njobs-_burnin))/(eTime[_nstages-1][_njobs]-eTime[_nstages-1][_burnin]);
 }
-
+//main method
 int main(int argc, char** argv){
     
     MPI_Init(NULL,NULL);
@@ -296,23 +297,12 @@ int main(int argc, char** argv){
         
         maxN = floor((maxS2 + sec_maxS2) * h2/lambda) + 1;
 
-	/**printf("max Index : %d\n", maxS2INDEX);
-	printf("max S2: %f\n", maxS2);
-
-	printf("second max Index: %d\n", sec_maxS2INDEX);
-	printf("second max S2: %f\n", sec_maxS2);        
-
-        printf("maxN is :%f\n",maxN);**/
+	
         
         for(int i = 1; i <= world_size - 1; i++){
             MPI_Send(&maxN,1, MPI_DOUBLE, i, 0, MPI_COMM_WORLD);
         }
-        /**
-         printf("First Stage Finished,%f\n",MPI_Wtime()-_starttime);
-         printf("maxS2 is %f\n",maxS2);
-         printf("Corresponding average is: %f\n",I[maxS2INDEX].sampleXSum/n0);
-         printf("h2 is %f\n",h2);
-         printf("maxN is %f\n",maxN);**/
+        
     }else{
         MPI_Recv(&maxN,1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
     }
